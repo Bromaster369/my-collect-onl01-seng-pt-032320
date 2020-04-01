9078a7bd09b173d2477bfe66410e68b37460d77d
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-01 13:33:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,9 @@
-
-def my_collect(collection) do |lang|
-  lang.upcase
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
